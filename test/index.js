91ee3da29646742f6dfbc2dee07bbab2924bcf74
Sebastian Real <seba.real.a@gmail.com>
2016-02-11 10:37:51 -0300

    adding new tests

--- test/index.js
+++ test/index.js
@@ -20,3 +20,38 @@ test('it should get a greeting value', function(assert){
   assert.equal(actual, expected, 'greeting should return ' + expected + ' but it returns ' + actual);
   assert.end();
 });
+
+test('it should get a greeting value, then get null', function(assert){
+  cookies.set('greeting', 'hola');
+
+  var actual = cookies.get('greeting', true);
+  var expected = 'hola';
+
+  assert.equal(actual, expected, 'greeting should return ' + expected + 'but it returns ' + actual);
+
+  actual = cookies.get('greeting');
+  expected = null;
+
+  assert.equal(actual, expected, 'greeting should return ' + expected + 'but it returns ' + actual);
+
+  assert.end();
+});
+
+test('it should get null when a inexistent key is given', function(assert){
+  var actual = cookies.get('playlist');
+  var expected = null;
+
+  assert.equal(actual, expected, 'it should return ' + expected + ' but it returns ' + actual);
+  assert.end();
+});
+
+test('it should remove a value when key is given', function(assert){
+  cookies.set('greeting', 'ciao');
+  cookies.remove('greeting');
+
+  var actual = cookies.get('greeting');
+  var expected = null;
+
+  assert.equal(actual, expected, 'it should return ' + expected + ' but it returns ' + actual);
+  assert.end();
+});
