75501ac83ff278572b0635c178f18b65a71fe036
Sebastian Real <seba.real.a@gmail.com>
2016-02-12 10:20:59 -0300

    update tests, npm scripts and README.md

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ If you need to retrieve a cookie value from cookies, just give it the key:
 
 ````javascript
 //document.cookie -> "foo=idunno; bar=firstValue"
+
 var myValue = cookies.get('foo');
 console.log('my value', myValue);//idunno
 
@@ -27,11 +28,14 @@ Also, you can just mark as deletable value(after it is recovered):
 
 ````javascript
 //document.cookie -> "foo=idunno; bar=firstValue"
+
 var myValue = cookies.get('foo', true);
 console.log(myValue);//idunno
 
 var otherValue = cookies.get('foo');
 console.log(otherValue);//null
+
+//document.cookie -> "bar=firstValue"
 ````
 
 
@@ -39,10 +43,11 @@ console.log(otherValue);//null
 
 cookies.set(key, value [, daysToExpire=365 ]);
 
-to set a value inside cookie, it is as easy as send it as follows
+to set a new cookie value, just give it the key and its value. Optionally, you can send how many days your cookie will survive.
 
 ````javascript
 cookies.set('coffeeType', 'mocha');
+
 //cookie="foo=idunno; bar=firstValue; coffeeType=mocha"
 ````
 
@@ -51,8 +56,12 @@ cookies.set('coffeeType', 'mocha');
 
 cookies.remove(key);
 
+To remove a cookie, just pass the key.
+
 ````javascript
 //cookie="foo=idunno; bar=firstValue"
+
 cookies.remove('foo');
+
 //cookie="bar=firstValue"
 ````
--- test/index.js
+++ test/index.js
@@ -1,28 +1,40 @@
 var test = require('tape');
 var cookies = require('../src/cookie');
 
+function setup(key, val){
+  cookies.set(key, val);
+}
+
+function teardown(key){
+  cookies.remove(key);
+}
+
 test('it should create a cookie with key name greeting', function(assert){
-  cookies.set('greeting', 'hello');
+  setup('greeting', 'hello');
 
   var actual = document.cookie.indexOf('greeting');
   var expected = -1;
 
   assert.notEqual(actual, expected, 'greeting actual code marked position : ' + actual);
   assert.end();
+
+  teardown('greeting');
 });
 
 test('it should get a greeting value', function(assert){
-  cookies.set('greeting', 'bonjour');
+  setup('greeting', 'bonjour');
 
   var actual = cookies.get('greeting');
   var expected = 'bonjour';
 
   assert.equal(actual, expected, 'greeting should return ' + expected + ' but it returns ' + actual);
   assert.end();
+
+  teardown('greeting');
 });
 
 test('it should get a greeting value, then get null', function(assert){
-  cookies.set('greeting', 'hola');
+  setup('greeting', 'hola');
 
   var actual = cookies.get('greeting', true);
   var expected = 'hola';
@@ -33,8 +45,9 @@ test('it should get a greeting value, then get null', function(assert){
   expected = null;
 
   assert.equal(actual, expected, 'greeting should return ' + expected + 'but it returns ' + actual);
-
   assert.end();
+
+  teardown('greeting');
 });
 
 test('it should get null when a inexistent key is given', function(assert){
@@ -46,7 +59,8 @@ test('it should get null when a inexistent key is given', function(assert){
 });
 
 test('it should remove a value when key is given', function(assert){
-  cookies.set('greeting', 'ciao');
+  setup('greeting', 'ciao');
+
   cookies.remove('greeting');
 
   var actual = cookies.get('greeting');
@@ -54,4 +68,6 @@ test('it should remove a value when key is given', function(assert){
 
   assert.equal(actual, expected, 'it should return ' + expected + ' but it returns ' + actual);
   assert.end();
+
+  teardown('greeting');
 });
